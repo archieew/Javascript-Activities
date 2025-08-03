4f53ed48c44850a19f8920a681375b84c7dcd5d9
Archie Custodio <archie06custodio@gmail.com>
2025-08-03 20:55:24 +0800

    Add files via upload

---
+++ Lab-Demo/node-example.js
@@ -0,0 +1,13 @@
+// Name:Custodio, Archie D.
+// Module: 3
+// Week 2 and 3: Basics of NodeJs
+
+//first program
+console.log("Welcome to WCSERVER!!");
+
+//Creating my first function
+function Display(x) {
+    console.log(x);
+}
+
+Display(200);  
